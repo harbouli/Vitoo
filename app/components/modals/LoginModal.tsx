c376165467be76c54cb2dff995a22c040b59752b
harbouli <mharbouli@icloud.com>
2023-05-30 13:20:52 +0100

    Update Project

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -97,7 +97,7 @@ const LoginModal = () => {
       text-neutral-500 text-center mt-4 font-light"
       >
         <p>
-          First time using Airbnb?
+          First time using Vitoo?
           <span
             onClick={onToggle}
             className="
